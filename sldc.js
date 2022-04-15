0378a90bef9418eb0f69238a933cc0135a6c025e
Esteban Pecina <estebanpecina14@gmail.com>
2022-04-15 11:47:58 -0500

    Finished development life cycle

--- sldc.js
+++ sldc.js
@@ -5,12 +5,13 @@
     Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
 */
 
+// Planning
 // Analysis of Requirements
 // Design
 // Implementation
-// Maintain
-// Planning
 // Testing & Integration
+// Maintain
+
 
 
 //////////////////PROBLEM 2////////////////////
@@ -18,4 +19,14 @@
     Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
     The first one is done for you. 
 */
-const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added."
+
+const analysisOfRequirments = "The second step, is defining requirements is considered part of planning to determine what the application is supposed to do and its requirements."
+
+const design = "The third step, the design phase models the way a software application will work."
+
+const implementation "The fourth step, is when implementation/coding starts once the developer gets the Design document. The Software design is translated into source code. All the components of the software are implemented in this phase."
+
+const testingAndIntegration "The fifth step, the objective of this phase is to perform system integration testing of the developed system. The systems integration test function is to ensure that the developed systems meet all the technical requirements with the components and subsystems integrated. The system test may require any number of additional tests depending on the scope and complexity of the requirements; examples include: security, conformance, accessibility, performance, stress, compatibility, and regression tests."
+
+const maintain = "The sixth step, in this phase the users discover bugs that weren’t found during testing. These errors need to be resolved, which can spawn new development cycles.In addition to bug fixes, models like Iterative development plan additional features in future releases. For each new release, a new Development Cycle can be launched."
