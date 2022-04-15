bab4ae86e7e6f85d2696ef1e8e2be90d6a3172bf
Esteban Pecina <estebanpecina14@gmail.com>
2022-04-15 12:31:44 -0500

    Turning in varibles.js

--- variables.js
+++ variables.js
@@ -2,16 +2,24 @@
 /* Create a variable called 'myName' with a value that is your name as a string. */
 
 //CODE HERE 
+const myName = 'Esteban'
+console.log(myName)
+
 
 //////////////////PROBLEM 2////////////////////
 /* Create a variable called 'faveNum' with a value that is your favorite number. */
 
 //CODE HERE
+let faveNum = 13;
+console.log(faveNum)
 
 //////////////////PROBLEM 3////////////////////
 /* Create a variable called 'lovesCode' and assign it a boolean value. */
 
 //CODE HERE
+const lovesCode = true;
+console.log(lovesCode)
+
 
 //////////////////PROBLEM 4////////////////////
 /*
@@ -21,6 +29,12 @@
 */
 
 //CODE HERE
+if(faveNum === 13) {
+  console.log('lucky 13!')
+} else {
+  console.log('not lucky 13.')
+}
+
 
 //////////////////PROBLEM 5////////////////////
 /*
@@ -28,3 +42,6 @@
 */
 
 //CODE HERE
+for(let i = 1; i <=5; i++); {
+  console.log(i)
+}
