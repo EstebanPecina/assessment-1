6de56c60234d5eac413c915ab024f04881f3c707
Esteban Pecina <estebanpecina14@gmail.com>
2022-04-18 02:06:03 -0500

    fixing password.js

--- password.js
+++ password.js
@@ -10,17 +10,28 @@
 
 // reader.question("Welcome to the password validator tool. What password would you like to validate?" , function(input) 
     
-// Going over work v//
+// New code Line
 
-const readline = require("readLine");
+const readline = require('readline');
 
 const reader = readline.createInterface({
-    input: processingInstruction.stidin,
-    output: processingInstruction.stdout
-})
+  input: process.stdin,
+  output: process.stdout
+});
+// set up for console interface //
+reader.question("Welcome to password validator tool. What password would you live to validate?", function(input){ //showing what question console will display//
 
-reader.question("Welcome to password validator tool.", function(input){ //This reader is going to read what we type in terminal//
-console.log(password);
+    console.log(input[0]);
 
-reader.cloer();
+    for (let i = 0; i <input.length; i ++) { //a for loop having it read the response length//
+        break;
+    }
+    
+    if (input.length >= 10) {     //setting requirement for password being equal or greater than 10//
+    console.log('password meets requirement')  
+}   else {                                      //easy way to console log for just one other outcome for password being less than 10//
+    console.log('password not long enough :(') 
+}
+
+reader.close(); 
 }) 
